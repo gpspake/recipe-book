9631f46dc151de6b77f9b540dd90b8b5871ea030
George Spake <gpspake@gmail.com>
2016-12-20 10:56:10 -0600

    add recipes property to recipe-list component with temporary recipe component for testing

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Recipe } from '../recipe';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [];
+  recipe = new Recipe('Dummy', 'Dummy Description', 'http://thedietfactory.net/wp-content/uploads/2013/02/gourmetfood.jpg');
   constructor() { }
 
   ngOnInit() {
